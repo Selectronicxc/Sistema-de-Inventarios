f615ff3d1eb1bcff0871005ace8ba4dfdfa67e81
Carlos Mejia <carlos.selectronics@gmail.com>
2018-03-25 19:48:43 -0600

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ function muestraPass() {
  		<div class="container">
  			<div class="row">
  				<div class="col-sm-8 col-sm-offset-3">
- 					<h2>AESYSTEM INVENTORY</h2>
+ 					<h2>AESYSTEM</h2>
  				</div>
  				<br>
  				<br>
